c9e3b8fea54134a65d20f506d9d854ee743191c6
mvaleriani <michaelvaleriani@gmail.com>
2018-07-06 15:24:31 -0700

    remove unnecessary log

--- content.js
+++ content.js
@@ -1,11 +1,9 @@
+document.addEventListener("scroll", e => {console.log("Scrolling")});
 let promotedEls = document.getElementsByClassName("fkuxEk")
 
 for (let i = 0; i < promotedEls.length; i++) {
     let currEl = promotedEls[i]
     let parentPost = currEl.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
     parentPost.setAttribute("style", "display: none");
-    console.log(parentPost);
-    
 }
 
-document.addEventListener("scroll", e => {console.log("Scrolling")});
