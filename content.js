6d284b86d591654b9adffbd03a7eca26212e9983
mvaleriani <michaelvaleriani@gmail.com>
2018-07-07 13:53:21 -0700

    add mutation tracker skele

--- content.js
+++ content.js
@@ -1,5 +1,33 @@
-document.addEventListener("fetch", e => {console.log("Scrolling")});
+document.addEventListener("animationend", e => {console.log("Scrolling")});
 let promotedEls = document.getElementsByClassName("fkuxEk")
+
+// Select the node that will be observed for mutations
+// var targetNode = document.getElementById('some-id');
+
+// Options for the observer (which mutations to observe)
+// var config = { attributes: true, childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+// var callback = function (mutationsList) {
+//     for (var mutation of mutationsList) {
+//         if (mutation.type == 'childList') {
+//             console.log('A child node has been added or removed.');
+//         }
+//         else if (mutation.type == 'attributes') {
+//             console.log('The ' + mutation.attributeName + ' attribute was modified.');
+//         }
+//     }
+// };
+
+// Create an observer instance linked to the callback function
+// var observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+// observer.observe(targetNode, config);
+
+// Later, you can stop observing
+// observer.disconnect();
+
 
 for (let i = 0; i < promotedEls.length; i++) {
     let currEl = promotedEls[i]
