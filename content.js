5b82a8da85e61136207e1de2e4c924dd4df49aa8
mvaleriani <michaelvaleriani@gmail.com>
2018-07-09 16:58:28 -0700

    infinite scroll remover working, still needs optimization

--- content.js
+++ content.js
@@ -1,37 +1,58 @@
-document.addEventListener("animationend", e => {console.log("Scrolling")});
-let promotedEls = document.getElementsByClassName("fkuxEk")
+
+
+// let promotedEls = document.getElementsByClassName("fkuxEk")
+
+// for (let i = 0; i < promotedEls.length; i++) {
+//     let currEl = promotedEls[i]
+//     let parentPost = currEl.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+//     parentPost.setAttribute("style", "display: none");
+// }
 
 // Select the node that will be observed for mutations
-// var targetNode = document.getElementById('some-id');
+let targetNode = document.getElementsByClassName("s1q3b4m-0")[0]
+// document.getElementById('some-id');
+// console.log("Num Posts: " + targetNode.children.length);
 
 // Options for the observer (which mutations to observe)
-// var config = { attributes: true, childList: true, subtree: true };
+let config = { attributes: true, childList: true, subtree: true };
 
 // Callback function to execute when mutations are observed
-// var callback = function (mutationsList) {
-//     for (var mutation of mutationsList) {
-//         if (mutation.type == 'childList') {
-//             console.log('A child node has been added or removed.');
-//         }
-//         else if (mutation.type == 'attributes') {
-//             console.log('The ' + mutation.attributeName + ' attribute was modified.');
-//         }
-//     }
-// };
+let callback = function (mutationsList) {
+    for (let mutation of mutationsList) {
+        if (mutation.type == 'childList') {
+            console.log('A child node has been added or removed.');
+            let arr = [];
+            let promotedEls = document.getElementsByClassName("ngb5qd-0");
+            let otherProm = document.getElementsByClassName("fkuxEk");
+
+            for (let j = 0; j < promotedEls.length; j++) {
+                arr.push(promotedEls[j]);
+            }
+            for (let k = 0; k < otherProm.length; k++) {
+                arr.push(otherProm[k]);
+            }
+            console.log(arr);
+            
+            // debugger;
+            // console.log(promotedEls);
+            
+            for (let i = 0; i < arr.length; i++) {
+                let currEl = arr[i]
+                let parentPost = currEl.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+                parentPost.remove();
+                // setAttribute("style", "display: none");
+            }
+        }
+    }
+};
 
 // Create an observer instance linked to the callback function
-// var observer = new MutationObserver(callback);
+let observer = new MutationObserver(callback);
 
 // Start observing the target node for configured mutations
-// observer.observe(targetNode, config);
+observer.observe(targetNode, config);
 
 // Later, you can stop observing
 // observer.disconnect();
 
-
-for (let i = 0; i < promotedEls.length; i++) {
-    let currEl = promotedEls[i]
-    let parentPost = currEl.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
-    parentPost.setAttribute("style", "display: none");
-}
 
